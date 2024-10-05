a3f056ae0946e9d8ce712d6fdff639982f025715
Camila Garcia <g.camila@correo.unimet.edu.ve>
2024-10-05 18:38:03 -0400

    Creacion de Dashboard , funcion txt, comentarios

--- src/Main/Director.java
+++ src/Main/Director.java
@@ -14,13 +14,14 @@ import java.util.logging.Logger;
  */
 public class Director extends Thread{
     public Semaphore luzTrafico;
-    public String estado; /*Para saber cuando trabaja o vigila*/
+    public String estado; //Para saber cuando trabaja o vigila
     public int salarioAcumulado;
     public int computadora;
     public int computadoraTG;
     public int tiempoDia;
     public Compania compania;
     
+    //Constructor con la información del director
     public Director(Compania compania, int tiempoDia, Semaphore luzTrafico){
         this.compania=compania;
         this.luzTrafico= luzTrafico;
@@ -28,23 +29,27 @@ public class Director extends Thread{
         this.salarioAcumulado=0;
     }
     
+    //Función cuando el director envia computadoras
     public void EstadoEnviandoComputadoras(){
         this.estado= "Envia computadoras";
     }
     
+    //Funcion cuando el director realiza labores administrativas
     public void EstadoDeTrabajo(){
         this.estado="Labores administrativas";
     }
     
+    //Funcion cuando vigila al pm
     public void EstadoVigilancia(){
         this.estado="Revisa que esta haciendo el Product Manager";
     }
     
-    
+    //Funcion que determina el pago del director 
     public void PagoDirector(){
         this.salarioAcumulado+=60*24;
     }
     
+    //Funcion que registra la nueva ganancia de los  computadores enviados 
     public void EnviaComputadoras(){
         if (Compania.tipo==0){
             this.compania.sumGanancias(this.compania.almacen.computadora*80000);
@@ -56,10 +61,10 @@ public class Director extends Thread{
     }
     
    
-    
+    //Funcion de director se encarga de enviar  todos los computadoras ya creados a las distribuidoras y de sus estados cambiantes
     public void Trabaja(){
         Random aleatorio = new Random();
-        int aleatorioNum= aleatorio.nextInt(25);/*0 del marcador mas 24 dias*/
+        int aleatorioNum= aleatorio.nextInt(25);//0 del marcador mas 24 dias
         
         try{
             if(this.compania.contador.tiempoRestante>0){
@@ -82,7 +87,8 @@ public class Director extends Thread{
             Logger.getLogger(Director.class.getName()).log(Level.SEVERE,null,ex);
         }
     }
-        
+    
+    //Funcion para ejecutar el trabajo y pago
     public void PagoTrabajo(){
         while(true){
             PagoDirector();
